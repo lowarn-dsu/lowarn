1d61363ad506f9efb1a4050d84972e4359c48d05
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-11-29 15:49:52 +0000

    Add empty GHC plugin (#25)

---
+++ lowarn/src/Lowarn/Plugin.hs
@@ -0,0 +1,14 @@
+module Lowarn.Plugin (plugin) where
+
+import GHC.Plugins
+
+plugin :: Plugin
+plugin =
+  defaultPlugin
+    { installCoreToDos = install
+    }
+
+install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+install _ todo = do
+  putMsgS "Lowarn plugin enabled."
+  return todo
