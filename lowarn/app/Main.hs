15d2d7bfe0d940af9b573321fcb8c37208d16402
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-12-18 12:45:38 +0000

    Update API usage in demo

--- lowarn/app/Main.hs
+++ lowarn/app/Main.hs
@@ -1,14 +1,35 @@
 module Main (main) where
 
 import Control.Monad (void)
+import Lowarn.ProgramId (ProgramId (ProgramId))
+import Lowarn.ProgramName (ProgramName (ProgramName))
+import Lowarn.ProgramVersion (ProgramVersion, parseWithDots)
 import Lowarn.Runtime (loadTransformer, loadVersion, runRuntime)
 import System.IO (stdin, stdout)
+
+mkProgramVersion :: String -> ProgramVersion
+mkProgramVersion = fromJust . listToMaybe . readP_to_S parseWithDots
+
+version1 :: ProgramVersion
+version1 = mkProgramVersion "1.0.0"
+
+version2 :: ProgramVersion
+version2 = mkProgramVersion "2.0.0"
+
+version3 :: ProgramVersion
+version3 = mkProgramVersion "3.0.0"
+
+followingName :: ProgramName
+followingName = ProgramName "following"
+
+followingId :: ProgramVersion -> ProgramId
+followingId = ProgramId followingName
 
 main :: IO ()
 main =
   runRuntime . void $
-    loadVersion "following" "1.0.0" (stdin, stdout)
-      >>= loadTransformer "following" "1.0.0" "2.0.0"
-      >>= loadVersion "following" "2.0.0"
-      >>= loadTransformer "following" "2.0.0" "3.0.0"
-      >>= loadVersion "following" "3.0.0"
+    loadVersion (followingId version1) (stdin, stdout)
+      >>= loadTransformer followingName version1 version2
+      >>= loadVersion (followingId version2)
+      >>= loadTransformer followingName version2 version3
+      >>= loadVersion (followingId version3)
