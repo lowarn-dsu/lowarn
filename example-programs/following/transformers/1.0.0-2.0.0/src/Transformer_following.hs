40c50c7635f3f6dfde6129b0adc8a47930baaedb
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-12-18 13:04:53 +0000

    Rename new to next

--- example-programs/following/transformers/1.0.0-2.0.0/src/Transformer_following.hs
+++ example-programs/following/transformers/1.0.0-2.0.0/src/Transformer_following.hs
@@ -8,11 +8,11 @@ where
 import Control.Arrow (first)
 import qualified Data.Sequence as Seq
 import qualified "lowarn-version-following-v1v0v0" Lowarn.ExampleProgram.Following as PreviousVersion
-import qualified "lowarn-version-following-v2v0v0" Lowarn.ExampleProgram.Following as NewVersion
+import qualified "lowarn-version-following-v2v0v0" Lowarn.ExampleProgram.Following as NextVersion
 import System.Random (mkStdGen, randomR)
 import System.Random.Stateful (applyIOGen, newIOGenM)
 
-transformer :: PreviousVersion.State -> IO (Maybe NewVersion.State)
+transformer :: PreviousVersion.State -> IO (Maybe NextVersion.State)
 transformer (PreviousVersion.State users in_ out) = do
   ioGen <- newIOGenM (mkStdGen 0)
   users' <-
@@ -20,8 +20,8 @@ transformer (PreviousVersion.State users in_ out) = do
       <$> mapM
         ( \(PreviousVersion.User nickname) ->
             applyIOGen
-              (first (NewVersion.User nickname) . randomR (1, 9999))
+              (first (NextVersion.User nickname) . randomR (1, 9999))
               ioGen
         )
         users
-  return $ Just $ NewVersion.State users' in_ out
+  return $ Just $ NextVersion.State users' in_ out
--- example-programs/following/transformers/2.0.0-3.0.0/src/Transformer_following.hs
+++ example-programs/following/transformers/2.0.0-3.0.0/src/Transformer_following.hs
@@ -7,13 +7,13 @@ where
 
 import Data.Foldable (toList)
 import qualified "lowarn-version-following-v2v0v0" Lowarn.ExampleProgram.Following as PreviousVersion
-import qualified "lowarn-version-following-v3v0v0" Lowarn.ExampleProgram.Following as NewVersion
+import qualified "lowarn-version-following-v3v0v0" Lowarn.ExampleProgram.Following as NextVersion
 
-transformer :: PreviousVersion.State -> IO (Maybe NewVersion.State)
+transformer :: PreviousVersion.State -> IO (Maybe NextVersion.State)
 transformer (PreviousVersion.State users in_ out) =
-  return $ Just $ NewVersion.State users' in_ out
+  return $ Just $ NextVersion.State users' in_ out
   where
     users' =
       reverse $
         toList $
-          fmap (NewVersion.User . PreviousVersion.showUser) users
+          fmap (NextVersion.User . PreviousVersion.showUser) users
