a7004c7df685bf6221e91841eb91d5281d735763
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-12-23 12:51:42 +0000

    Remove unnecessary brackets

--- examples/following/demo/app/Main.hs
+++ examples/following/demo/app/Main.hs
@@ -14,14 +14,14 @@ main :: IO ()
 main =
   runRuntime $ do
     state1 <-
-      loadVersion (followingVersionId_1) Nothing
+      loadVersion followingVersionId_1 Nothing
 
     updatePackageDatabase
     state2 <-
-      loadVersion (followingVersionId_2)
+      loadVersion followingVersionId_2
         =<< loadTransformer followingTransformerId_1_2 state1
 
     updatePackageDatabase
     void $
-      loadVersion (followingVersionId_3)
+      loadVersion followingVersionId_3
         =<< loadTransformer followingTransformerId_2_3 state2
