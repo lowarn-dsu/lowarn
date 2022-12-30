613c452291dede86a95b05c124539b38d2fc2e7b
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-12-30 18:22:48 +0000

    Improve `Test.Lowarn.Tasty` docs

--- core-test/src/Test/Lowarn/Tasty.hs
+++ core-test/src/Test/Lowarn/Tasty.hs
@@ -4,13 +4,16 @@
 -- Stability               : stable
 -- Portability             : portable
 --
--- Module for utilities for Tasty.
+-- Module for utilities for using Tasty.
 module Test.Lowarn.Tasty (BinarySemaphore, withBinarySemaphore) where
 
 import Control.Concurrent (MVar, newMVar)
 import Test.Tasty (TestTree, withResource)
 
+-- | A binary semaphore.
 type BinarySemaphore = MVar ()
 
+-- | Run a test that requires a binary semaphore. The @'IO' a@ action will
+-- give the same semaphore every time it is run.
 withBinarySemaphore :: (IO BinarySemaphore -> TestTree) -> TestTree
 withBinarySemaphore = withResource (newMVar ()) (const $ return ())
