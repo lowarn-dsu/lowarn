a3802f52553b1022a764cff28105a40f758bddda
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-02-12 16:49:14 +0000

    Add injection type classes

---
+++ inject/src/Lowarn/Inject.hs
@@ -0,0 +1,28 @@
+-- |
+-- Module                  : Lowarn.Inject
+-- SPDX-License-Identifier : MIT
+-- Stability               : experimental
+-- Portability             : non-portable (POSIX, GHC)
+--
+-- Module for a plugin used to inject runtime data.
+module Lowarn.Inject
+  ( InjectRuntimeData,
+    injectRuntimeData,
+    InjectedRuntimeData,
+    injectedRuntimeData,
+  )
+where
+
+import Lowarn (RuntimeData)
+
+class InjectRuntimeData a where
+  injectRuntimeData' :: RuntimeData a -> IO ()
+
+injectRuntimeData :: InjectRuntimeData a => RuntimeData a -> IO ()
+injectRuntimeData = injectRuntimeData'
+
+class InjectedRuntimeData a where
+  injectedRuntimeData' :: RuntimeData a
+
+injectedRuntimeData :: InjectedRuntimeData a => RuntimeData a
+injectedRuntimeData = injectedRuntimeData'
