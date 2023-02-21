7e8f7ea58cb22539280b50849859f1de23bcbf20
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-02-21 18:01:35 +0000

    Add EntryPoint module checking

--- inject/src/Lowarn/Inject/Plugin.hs
+++ inject/src/Lowarn/Inject/Plugin.hs
@@ -10,24 +10,19 @@
 -- Module for a plugin used to inject runtime data.
 module Lowarn.Inject.Plugin (plugin) where
 
+import Control.Applicative
 import Control.Monad (guard, liftM4)
+import Data.Foldable (maximumBy)
 import Data.Maybe (mapMaybe)
+import Data.Ord (comparing)
 import GHC.Core.Predicate
-import GHC.Data.IOEnv (getEnv, liftIO)
-import GHC.Plugins
-  ( ModIface_ (mi_exports, mi_globals),
-    Plugin (..),
-    defaultPlugin,
-    getOccString,
-    lookupGlobalRdrEnv,
-    mkCoreApps,
-    purePlugin,
-  )
-import GHC.Tc.Module (getModuleInterface)
-import GHC.Tc.Types (Env (env_top), tcg_mod)
+import GHC.Plugins hiding (TcPlugin)
+import GHC.Tc.Types (tcg_mod)
 import GHC.TcPlugin.API
-import GHC.TcPlugin.API.Internal (unsafeWithRunInTcM)
-import GHC.Types.Avail (availName)
+import Lowarn.ParserCombinators (parsePackageName, readWithParser)
+import Lowarn.ProgramName (parseEntryPointModuleName)
+import Lowarn.VersionId (parseVersionPackageName, _programName)
+import Text.ParserCombinators.ReadP (readP_to_S)
 import Text.Printf (printf)
 
 plugin :: Plugin
@@ -72,47 +67,36 @@ solveClassConstraint resolvedNames (ct, t) =
       ct
     )
 
-moduleHasRuntimeDataVar :: Module -> TcPluginM 'Init Bool
-moduleHasRuntimeDataVar entryPointModule = unsafeWithRunInTcM $ \_ -> do
-  session <- env_top <$> getEnv
-  (_, mInterface) <- liftIO $ getModuleInterface session entryPointModule
-  return $ case mInterface >>= mi_globals of
-    Just globals ->
-      not $ null $ lookupGlobalRdrEnv globals (mkVarOcc "runtimeDataVar")
-    Nothing -> False
-
-moduleHasRuntimeDataVar' :: Module -> TcPluginM 'Init Bool
-moduleHasRuntimeDataVar' entryPointModule = unsafeWithRunInTcM $ \_ -> do
-  session <- env_top <$> getEnv
-  (_, mInterface) <- liftIO $ getModuleInterface session entryPointModule
-  return $ case mInterface of
-    Just interface ->
-      any
-        ( \availInfo ->
-            getOccString (availName availInfo) == "runtimeDataVar"
-        )
-        $ mi_exports interface
-    Nothing -> False
-
 getClasses :: TcPluginM 'Init (Maybe ResolvedNames)
 getClasses = do
   currentModule <- tcg_mod . fst <$> getEnvs
+  let mModuleProgramName =
+        readWithParser
+          parseEntryPointModuleName
+          (moduleNameString $ moduleName currentModule)
+      currentUnitId = unitString $ moduleUnit currentModule
+      mPackageName = case map fst $ readP_to_S parsePackageName currentUnitId of
+        [] -> Nothing
+        possiblePackageNames ->
+          Just $ maximumBy (comparing length) possiblePackageNames
+      mPackageProgramName =
+        _programName
+          <$> (readWithParser parseVersionPackageName =<< mPackageName)
 
-  moduleHasRuntimeDataVar' currentModule
-    >>= \case
-      True -> do
-        injectModule <-
-          findModule "Lowarn.Inject" $ Just "lowarn-inject"
-        runtimeDataVarModule <-
-          findModule "Lowarn.Inject.RuntimeDataVar" $ Just "lowarn-inject"
-        Just
-          <$> liftM4
-            ResolvedNames
-            (getClass injectModule "InjectedRuntimeData")
-            (getClass injectModule "InjectRuntimeData")
-            (getId runtimeDataVarModule "putRuntimeDataVar")
-            (getId runtimeDataVarModule "runtimeDataVar")
-      False -> return Nothing
+  case liftA2 (,) mModuleProgramName mPackageProgramName of
+    Just (moduleProgramName, packageProgramName)
+      | moduleProgramName == packageProgramName -> do
+          injectModule <- findModule "Lowarn.Inject" $ Just "lowarn-inject"
+          runtimeDataVarModule <-
+            findModule "Lowarn.Inject.RuntimeDataVar" $ Just "lowarn-inject"
+          Just
+            <$> liftM4
+              ResolvedNames
+              (getClass injectModule "InjectedRuntimeData")
+              (getClass injectModule "InjectRuntimeData")
+              (getId runtimeDataVarModule "putRuntimeDataVar")
+              (getId currentModule "runtimeDataVar")
+    _ -> return Nothing
   where
     findModule :: String -> Maybe String -> TcPluginM 'Init Module
     findModule moduleNameToFind mPackageNameToFind =
