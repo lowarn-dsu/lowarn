82834ac877424ae81a729107b6337e400426add1
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-03-05 23:07:46 +0000

    Add YAML parsing

--- cli/app/Main.hs
+++ cli/app/Main.hs
@@ -1,5 +1,50 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
+
+import Data.Yaml (decodeFileEither)
+import Lowarn.Cli.Config
+import Lowarn.ProgramName
+import Options.Applicative
+import Text.Printf
+
+newtype Options = Options
+  { optionsLowarnYamlPath :: Maybe FilePath
+  }
+
+parser :: Parser Options
+parser = do
+  optionsLowarnYamlPath <-
+    optional $
+      strOption $
+        long "lowarn-yaml"
+          <> metavar "LOWARN-YAML"
+          <> help "Override Lowarn CLI configuration file."
+  return Options {..}
+
+parserInfo :: ParserInfo Options
+parserInfo =
+  info (helper <*> parser) $
+    fullDesc
+      <> progDesc "Run programs that support DSU with Lowarn"
+      <> header
+        "lowarn-cli - a CLI for running programs that support DSU with Lowarn"
 
 main :: IO ()
 main = do
-  putStrLn "Lowarn CLI"
+  Options {..} <- execParser parserInfo
+  lowarnYamlPath <- case optionsLowarnYamlPath of
+    Just p -> return p
+    Nothing ->
+      fail
+        "Automatically finding the Lowarn configuration file is not currently supported."
+  decodeFileEither lowarnYamlPath >>= \case
+    Left e ->
+      printf "Could not parse Lowarn CLI configuration file:\n%s" (show e)
+    Right config -> do
+      putStrLn $
+        printf "Program name is %s." $
+          unProgramName $
+            lowarnConfigProgramName config
---
+++ cli/src/Lowarn/Cli/Config.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Module                  : Lowarn.Cli.Config
+-- SPDX-License-Identifier : MIT
+-- Stability               : stable
+-- Portability             : non-portable (GHC)
+--
+-- Module for Lowarn CLI configurations.
+module Lowarn.Cli.Config (LowarnConfig (..)) where
+
+import Data.Yaml
+import Lowarn.ProgramName
+import Lowarn.ProgramName.Aeson ()
+
+-- | Type for Lowarn CLI configurations.
+newtype LowarnConfig = LowarnConfig
+  { -- | The name of the program that is being updated.
+    lowarnConfigProgramName :: ProgramName
+  }
+  deriving (Show)
+
+instance FromJSON LowarnConfig where
+  parseJSON :: Value -> Parser LowarnConfig
+  parseJSON = withObject "LowarnConfig" $ \v ->
+    LowarnConfig
+      <$> v
+      .: "program-name"
