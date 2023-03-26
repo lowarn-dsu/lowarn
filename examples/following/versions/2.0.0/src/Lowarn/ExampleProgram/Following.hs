7d1c6cb90ddcb3ef8a4f2ecaaaa20542fa313d4c
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-03-26 16:35:02 +0100

    Remove User type from new version

--- examples/following/versions/2.0.0/src/Lowarn/ExampleProgram/Following.hs
+++ examples/following/versions/2.0.0/src/Lowarn/ExampleProgram/Following.hs
@@ -1,26 +1,13 @@
-module Lowarn.ExampleProgram.Following
-  ( User (..),
-    State (..),
-    eventLoop,
-    showUser,
-  )
-where
-
-newtype User = User
-  { userName :: String
-  }
+module Lowarn.ExampleProgram.Following (State (..), eventLoop) where
 
 newtype State = State
-  { stateUsers :: [User]
+  { stateUsers :: [String]
   }
-
-showUser :: User -> String
-showUser = userName
 
 eventLoop :: State -> IO State
 eventLoop (State users) = do
   putStrLn "Following (2):"
-  mapM_ (putStrLn . showUser) users
+  mapM_ putStrLn users
   putStrLn "------"
-  user <- User <$> getLine
+  user <- getLine
   eventLoop $ State (user : users)
