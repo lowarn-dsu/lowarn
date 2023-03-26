114654c14ad8d53dd85df0aff770b78188b0046f
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-03-26 12:51:49 +0100

    Remove use of transformers

--- examples/following/updates/1.0.0-2.0.0/src/Update_following.hs
+++ examples/following/updates/1.0.0-2.0.0/src/Update_following.hs
@@ -10,30 +10,30 @@ import Control.Arrow (first)
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 import Foreign (StablePtr, newStablePtr)
-import Lowarn (Update (Update))
-import Lowarn.Transformer (Transformable (transform))
-import Lowarn.Transformer.Strict (StrictTransformable (transformer'))
+import Lowarn (Transformer (Transformer), Update (Update))
 import System.Random (mkStdGen, randomR)
 import System.Random.Stateful (applyIOGen, newIOGenM)
 import qualified "lowarn-version-following-v1v0v0" Lowarn.ExampleProgram.Following as PreviousVersion
 import "lowarn-version-following-v2v0v0" EntryPoint_following (entryPoint)
 import qualified "lowarn-version-following-v2v0v0" Lowarn.ExampleProgram.Following as NextVersion
 
-instance Transformable [PreviousVersion.User] (Seq NextVersion.User) where
-  transform :: [PreviousVersion.User] -> IO (Maybe (Seq NextVersion.User))
-  transform users = do
+transformer :: Transformer PreviousVersion.State NextVersion.State
+transformer = Transformer $
+  \(PreviousVersion.State users inHandle outHandle) -> do
     ioGen <- newIOGenM (mkStdGen 0)
-    Just . Seq.fromList
-      <$> mapM
-        ( \(PreviousVersion.User nickname) ->
-            applyIOGen
-              (first (NextVersion.User nickname) . randomR (1, 9999))
-              ioGen
-        )
-        users
+    users' <-
+      Seq.fromList
+        <$> mapM
+          ( \(PreviousVersion.User nickname) ->
+              applyIOGen
+                (first (NextVersion.User nickname) . randomR (1, 9999))
+                ioGen
+          )
+          users
+    return $ Just $ NextVersion.State users' inHandle outHandle
 
 foreign export ccall "hs_update_v1v0v0_v2v0v0"
   hsUpdate :: IO (StablePtr (Update PreviousVersion.State NextVersion.State))
 
 hsUpdate :: IO (StablePtr (Update PreviousVersion.State NextVersion.State))
-hsUpdate = newStablePtr $ Update transformer' entryPoint
+hsUpdate = newStablePtr $ Update transformer entryPoint
